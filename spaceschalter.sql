2486ca9ab9d2ae5cdc3465f411647e00b69c1968
holger <holger@local>
2013-06-23 19:19:52 +0200

    db schema added

---
+++ spaceschalter.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE IF NOT EXISTS `devices` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `devices` int(11) NOT NULL DEFAULT '0',
+  `people` int(11) NOT NULL DEFAULT '0',
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `spacestate` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `state` varchar(50) NOT NULL,
+  `until` datetime DEFAULT NULL,
+  `lastupdate` datetime DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
