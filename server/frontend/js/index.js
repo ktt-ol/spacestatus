3a32f567107261b0d4f7aa1d3689f76d4a2c8cb7
holger <holger@local>
2013-06-25 22:18:05 +0200

    people in space updated; more documentated

--- server/frontend/js/index.js
+++ server/frontend/js/index.js
@@ -85,8 +85,8 @@ function onDomReady() {
   source.addEventListener('spaceDevices', function (e) {
     var data = $.parseJSON(e.data);
 
-    var text = data.deviceCount + '/' + data.peopleCount;
-    var title = data.people.join(', ');
+    var text = data.peopleCount + '/' + data.deviceCount;
+    var title = data.people.length > 0 ? data.people.join(', ') : 'No public people';
     $('#pis').text(text).attr('title', title);
     timestamps.spaceDevices = data.timestamp;
   }, false);
