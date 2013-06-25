fe1aa717bb50a0bf2ebe9f12f45ce434a3a00aa3
holger <holger@local>
2013-06-25 21:32:46 +0200

    maxListener increased

--- server/backend/service/events.js
+++ server/backend/service/events.js
@@ -3,49 +3,60 @@
 var LOG = require('./../aspects/loggerFactory.js').logger();
 var EventEmitter = require('events').EventEmitter;
 
-module.exports = {
-  EVENT: {
-    SPACE_OPEN: 'space_open',
-    SPACE_DEVICES: 'spaceDevices',
-    FREIFUNK: 'freifunk',
-    WEATHER: 'weather'
-  },
+module.exports = function () {
 
-  _events: new EventEmitter(),
+  var exportObj = {
+    EVENT: {
+      SPACE_OPEN: 'space_open',
+      SPACE_DEVICES: 'spaceDevices',
+      FREIFUNK: 'freifunk',
+      WEATHER: 'weather'
+    },
 
-  _validateEventName: function (eventNmae) {
-    var allEvents = module.exports.EVENT;
-    for (var key in allEvents) {
-      if (allEvents.hasOwnProperty(key) && allEvents[key] === eventNmae) {
-        return;
+    _events: new EventEmitter(),
+
+    init: function () {
+      this._events.setMaxListeners(250);
+    },
+
+    _validateEventName: function (eventNmae) {
+      var allEvents = exportObj.EVENT;
+      for (var key in allEvents) {
+        if (allEvents.hasOwnProperty(key) && allEvents[key] === eventNmae) {
+          return;
+        }
       }
-    }
-    throw new Error('Invalid event name.');
-  },
+      throw new Error('Invalid event name.');
+    },
 
-  on: function (eventName, callback) {
-    this._validateEventName(eventName);
-    this._events.on(eventName, callback);
-  },
+    on: function (eventName, callback) {
+      this._validateEventName(eventName);
+      this._events.on(eventName, callback);
+    },
 
-  remove: function (eventName, callback) {
-    this._validateEventName(eventName);
-    this._events.removeListener(eventName, callback);
-  },
+    remove: function (eventName, callback) {
+      this._validateEventName(eventName);
+      this._events.removeListener(eventName, callback);
+    },
 
-  removeAll: function (callback) {
-    var allEvents = module.exports.EVENT;
-    for (var key in allEvents) {
-      if (allEvents.hasOwnProperty(key)) {
-        this.remove(allEvents[key], callback);
+    removeAll: function (callback) {
+      var allEvents = exportObj.EVENT;
+      for (var key in allEvents) {
+        if (allEvents.hasOwnProperty(key)) {
+          this.remove(allEvents[key], callback);
+        }
       }
+    },
+
+    emit: function (eventName, data) {
+      LOG.debug('Event: ' + eventName);
+
+      this._validateEventName(eventName);
+      this._events.emit(eventName, data);
     }
-  },
+  };
 
-  emit: function (eventName, data) {
-    LOG.debug('Event: ' + eventName);
+  exportObj._events.setMaxListeners(250);
 
-    this._validateEventName(eventName);
-    this._events.emit(eventName, data);
-  }
+  return exportObj;
 };
--- server/backend/service/index.js
+++ server/backend/service/index.js
@@ -7,11 +7,11 @@ module.exports = {
 
     var Twitter = require('./twitter.js');
     var Xmpp = require('./xmpp.js');
-    var events = require('./events.js');
+    var Events = require('./events.js');
 
     srv.twitter = new Twitter(config.twitter);
     srv.xmpp = new Xmpp(config.xmpp, data.state);
-    srv.events = events;
+    srv.events = new Events();
 
     return srv;
   }
