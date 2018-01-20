2d35a8674479ad9134b548d72cdef180f64b9583
Holger Cremer <HolgerCremer@gmail.com>
2018-01-20 19:51:48 +0100

    adss the location info with colors

--- client/app/main/person.directive.js
+++ client/app/main/person.directive.js
@@ -4,27 +4,22 @@ angular.module('status2App').directive('person', function () {
 
   return {
     // language=HTML
-    template: '<span class="name">{{::name}}</span>\n<span class="devices text-muted" ng-if="devices"><small>{{::devices}}</small></span>',
+    template: '<span class="name">{{::name}}</span>\n<span class="devices text-muted" ng-if="devices.length > 0">[<small ng-repeat="d in ::devices" ng-class="::d.location" class="location">{{::d.name}}</small>]</span>',
     restrict: 'E',
     scope: {
       p: '&'
     },
     link: function (scope, element, attrs) {
       var p = scope.p();
-
       scope.name = p.name;
 
-      var devices = [];
+      scope.devices = [];
       if (p.devices) {
         p.devices.forEach(function (device) {
           if (device.name !== '') {
-            devices.push(device.name);
+            scope.devices.push(device);
           }
         });
-
-        if (devices.length > 0) {
-          scope.devices = '[' + devices.join(', ') + ']';
-        }
       }
     }
   };
